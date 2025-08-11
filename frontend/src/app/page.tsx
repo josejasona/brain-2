d239567c225300f1403a45d1e9ac9dd872efd226
josejasona <jose.jason.a@gmail.com>
2025-08-11 15:57:29 -0400

    added shadcn and submit functionality on frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,16 @@
 "use client"
 import { useState } from "react"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
 const [query, setQuery] = useState("")
+const handleSubmit = async () => {
+   console.log(query)
 
+
+
+
+}
 return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
@@ -13,9 +20,11 @@ return (
         <input
             type="text"
             placeholder="Search..."
-            className="w-full max-w-xl px-4 py-2 rounded-full border border-gray-300 bg-gray-600  shadow-sm focus:border-gray-8000 focus:shadow-md transition"
-         />
-       
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+            className="w-full max-w-xl px-4 py-2 rounded-full border border-gray-300 shadow-sm focus:border-gray-8000 focus:shadow-md transition"
+         /> 
+         <Button onClick={handleSubmit}> Submit </Button>      
        </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
